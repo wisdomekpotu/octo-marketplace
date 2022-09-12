eb1dc804f607e54b2c19f91f671e60fa05db752f
wisdom ekpotu <whizfidel@gmail.com>
2022-09-12 04:55:16 +0100

    added tawilwind css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>Header</div>
+    </>
+  );
 }
 
 export default Header;
