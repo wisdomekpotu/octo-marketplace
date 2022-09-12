db60a64f89e97893e50d195f3e7012afa5d73c59
wisdom ekpotu <whizfidel@gmail.com>
2022-09-12 04:48:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
