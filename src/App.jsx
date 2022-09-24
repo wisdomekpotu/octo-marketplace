3e4e69f0c21f58dd2daef9b7c0343917415caf25
wisdom ekpotu <whizfidel@gmail.com>
2022-09-24 19:08:53 +0100

    made improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import dashboard from './pages/dashboard';
+import signin from './pages/signin';
+import Banner from './components/Banner/Banner';
 import './App.css';
+import ProductCard from './components/ProductCard/ProductCard';
 
 function App() {
   return (
     <div className='App'>
+      <Banner />
       <Header />
-      <Footer />
+      <ProductCard />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Dashboard from './pages/dashboard';
+import ErrorPage from './pages/error-page';
+import Signin from './pages/signin';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+
+  {
+    path: '/onboard',
+    element: <Signin />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
